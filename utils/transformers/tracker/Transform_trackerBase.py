9bcfd787727b760c51074de947d8076ee54aebf8
Zhipeng Tang <zhipengtang@cs.umass.edu>
2021-10-12 20:55:26 -0400

    add hide_bbox arg

--- utils/transformers/tracker/Transform_trackerBase.py
+++ utils/transformers/tracker/Transform_trackerBase.py
@@ -7,17 +7,18 @@ class Transform_trackerBase(Transform_base):
     def __init__(self):
         super().__init__()
     
-    def visualizeImgWithBbox(self, frame, bbox):
+    def visualizeImgWithBbox(self, frame, bbox, hide_bbox=False):
         frame = np.uint8(frame)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.rectangle(frame,
-                              (bbox[0], bbox[1]),
-                              (bbox[0] + bbox[2], bbox[1] + bbox[3]),
-                              (255, 0, 0), 2)
+        if not hide_bbox:
+            frame = cv2.rectangle(frame,
+                                (bbox[0], bbox[1]),
+                                (bbox[0] + bbox[2], bbox[1] + bbox[3]),
+                                (255, 0, 0), 2)
 
         return frame
 
-    def visualizeBboxInFixedPosition(self, frame, bbox, init_frame, init_bbox):
+    def visualizeBboxInFixedPosition(self, frame, bbox, init_frame, init_bbox, hide_bbox=False):
         x0, y0, w, h = bbox
         bbox_ = np.array([[x0, y0], [x0+w, y0], [x0+w, y0+h], [x0, y0+h]])
         x0, y0, w, h = init_bbox
@@ -26,8 +27,9 @@ class Transform_trackerBase(Transform_base):
         ha, status = cv2.findHomography(bbox_, init_bbox_)
         frame_warped = cv2.warpPerspective(frame, ha, (init_frame.shape[1], init_frame.shape[0]), borderMode=cv2.BORDER_REPLICATE)
         
-        p1 = (int(init_bbox[0]), int(init_bbox[1]))
-        p2 = (int(init_bbox[0] + init_bbox[2]), int(init_bbox[1] + init_bbox[3]))
-        cv2.rectangle(frame_warped, p1, p2, (255,0,0), 2, 1)
+        if not hide_bbox:
+            p1 = (int(init_bbox[0]), int(init_bbox[1]))
+            p2 = (int(init_bbox[0] + init_bbox[2]), int(init_bbox[1] + init_bbox[3]))
+            cv2.rectangle(frame_warped, p1, p2, (255,0,0), 2, 1)
 
         return frame_warped
--- utils/transformers/tracker/Transform_trackerFramework.py
+++ utils/transformers/tracker/Transform_trackerFramework.py
@@ -10,8 +10,9 @@ class Transform_trackerFramework(Transform_trackerBase):
 
     def getArgParser(self):
         parser = argparse.ArgumentParser(description='Base argument parser for tracker')
-        parser.add_argument('--vis', type=str, default='img', help='the visualization method (img/bboxonly/bboxfixed).')
+        parser.add_argument('--vis', type=str, default='img', help='the visualization method (img/bboxfixed).')
         parser.add_argument('--bbox', nargs='+', type=int, help='the coordinates of the initial bounding box.')
+        parser.add_argument('--hide_bbox', type=bool, default=False, help='hide the bbox in the output images.')
         return parser
 
     def processImageCollection(self, model, args):
@@ -28,11 +29,9 @@ class Transform_trackerFramework(Transform_trackerBase):
             # yield self.visualize(img_np, bbox, args.bbox, args.vis), img_name
 
             if args.vis == 'img':
-                yield self.visualizeImgWithBbox(img_np, bbox), img_name
-            elif args.vis == 'bboxonly':
-                raise NotImplemented()
+                yield self.visualizeImgWithBbox(img_np, bbox, hide_bbox=args.hide_bbox), img_name
             elif args.vis == 'bboxfixed':
-                yield self.visualizeBboxInFixedPosition(img_np, bbox, model.getImg(0), args.bbox), img_name
+                yield self.visualizeBboxInFixedPosition(img_np, bbox, model.getImg(0), args.bbox, hide_bbox=args.hide_bbox), img_name
             else:
                 raise ValueError('invalid vis argument.')
 
